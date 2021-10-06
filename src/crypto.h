d7862c20557d19597dc1652349a761ca1998a603
Stephen Lombardo <sjlombardo@zetetic.net>
2021-10-06 16:26:26 -0400

    update version to 4.5.0

--- src/crypto.h
+++ src/crypto.h
@@ -59,7 +59,7 @@ void sqlite3pager_reset(Pager *pPager);
 #define CIPHER_STR(s) #s
 
 #ifndef CIPHER_VERSION_NUMBER
-#define CIPHER_VERSION_NUMBER 4.4.3
+#define CIPHER_VERSION_NUMBER 4.5.0
 #endif
 
 #ifndef CIPHER_VERSION_BUILD
